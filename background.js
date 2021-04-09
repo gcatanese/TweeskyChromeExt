419822d222a5fa1253c06844a92d961921987a3b
perosa <beppe.catanese@gmail.com>
2021-04-09 14:28:50 +0200

    Implement open tab with URL

---
+++ background.js
@@ -0,0 +1,7 @@
+chrome.browserAction.onClicked.addListener(function(activeTab) {
+    //var newURL = "http://localhost:3000?target=" + activeTab.url;
+    var newURL = "https://tweesky.com/?target=" + activeTab.url;
+    chrome.tabs.create({
+        url: newURL
+    });
+});
