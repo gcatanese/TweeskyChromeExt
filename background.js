73529bc563a19e18da90895e9e4d5f4640e54fac
perosa <beppe.catanese@gmail.com>
2021-04-11 16:06:14 +0200

    Config prod URL

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(activeTab) {
-    //var newURL = "http://localhost:3000?target=" + activeTab.url;
+//    var newURL = "http://localhost:3000?target=" + activeTab.url;
     var newURL = "https://tweesky.com/?target=" + activeTab.url;
     chrome.tabs.create({
         url: newURL
