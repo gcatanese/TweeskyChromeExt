b8695933dbf29db6f8d11bf9050839fa96722e20
Beppe Catanese <1771700+gcatanese@users.noreply.github.com>
2023-01-04 13:52:46 +0100

    Change domain

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.browserAction.onClicked.addListener(function(activeTab) {
 //    var newURL = "http://localhost:3000?target=" + activeTab.url;
-    var newURL = "https://tweesky.com/?target=" + activeTab.url;
+    var newURL = "https://tweesky.online/?target=" + activeTab.url;
     chrome.tabs.create({
         url: newURL
     });
